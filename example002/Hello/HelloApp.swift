62801c2845cd734b4886d4a833b78d9dfd4a037d
arick <arick@grr.la>
2023-03-04 22:15:37 +0800

    test

---
+++ example002/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by user on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
